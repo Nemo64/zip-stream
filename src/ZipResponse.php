7a56a8834c0eedb06f9bb935f9d2034ae9047870
Marco Pfeiffer <git@marco.zone>
2019-01-28 12:27:21 +0100

    ensure that the size of the zip is precise

--- src/ZipResponse.php
+++ src/ZipResponse.php
@@ -15,14 +15,20 @@ class ZipResponse
 
     public static function create(ZipStream $stream, string $filename): ResponseInterface
     {
-        $filenameFallback = preg_replace("/[^a-zA-Z0-9\\._-]/", '_', $filename);
+        $headers = [];
 
-        $headers = [
-            'Content-Disposition' => $filenameFallback !== $filename
-                ? "attachment; filename=$filenameFallback; filename*=UTF-8''" . urlencode($filename)
-                : "attachment; filename=$filenameFallback",
-            'Content-Length' => $stream->getSize(),
-        ];
+        $filenameFallback = preg_replace("/[^a-zA-Z0-9\\._-]/", '_', $filename);
+        $headers['Content-Disposition'] = $filenameFallback !== $filename
+            ? "attachment; filename=$filenameFallback; filename*=UTF-8''" . urlencode($filename)
+            : "attachment; filename=$filenameFallback";
+
+        $size = $stream->getSize();
+        if (is_int($size) && $size <= PHP_INT_MAX && $size > 0) {
+            $headers['Content-Length'] = $size;
+        } else {
+            $errorMsg = "$filename exceeds PHP_INT_MAX and is therefor not precise. Content-Length is omitted.";
+            trigger_error($errorMsg, E_USER_WARNING);
+        }
 
         return new Response(200, $headers, $stream);
     }
